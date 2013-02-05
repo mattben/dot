51bc42575ad4bbef2c8b204641ff084448e88c7c
Matthew Harris <mattbenharris@gmail.com>
2013-02-05 09:31:00 -0800

    MBH: cleaning up bash

--- bash/brewUpgrade.sh
+++ bash/brewUpgrade.sh
@@ -10,9 +10,10 @@ echo "go brew cleanup";
 brew cleanup;
 
 for d in $(find . -maxdepth 1 -mindepth 1 -type d); do
+  $name=echo -e "$d";
   echo -e "$d";
-  brew upgrade $d;
+  brew upgrade $name;
   echo "***********************************************************";
 done
-
+brew cleanup;
 echo "Done";
